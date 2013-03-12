6ea7699930937505ce60edb45b9151466ca5fdde
Mohit Kanwal <mohit.kanwal@gmail.com>
2013-03-13 00:51:07 +0800

    Adding Stuff for QNetwork and JSON

--- panexapi/panexapi.cpp
+++ panexapi/panexapi.cpp
@@ -1,6 +1,47 @@
 #include "panexapi.h"
+#include <QtNetwork>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QUrl>
+
+const QString PanexApi::UrlPanex = "http://localhost:3000";
+const QString PanexApi::UrlUserSignUp = PanexApi::UrlPanex + "/users/sign_up.json";
+const QString PanexApi::UrlUserLogin = PanexApi::UrlPanex + "/users/sign_in.json";
+
+
+// Need this for singleton purposes
+PanexApi* PanexApi::m_panex_api=0;
 
 PanexApi::PanexApi(QObject *parent) :
     QObject(parent)
 {
+}
+
+PanexApi* PanexApi::instance()
+{
+    return PanexApi::m_panex_api;
+}
+
+bool PanexApi::SignUpUser(QString userName, QString userPassword, QString userRole, QString userEmail)
+{
+    QNetworkAccessManager networkManager;
+    QUrl url(PanexApi::UrlUserSignUp);
+//    QUrl url(SYNC_URL);
+//    QNetworkRequest request(url);
+
+//    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+//    QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+
+//    connect(manager, SIGNAL(finished(QNetworkReply*)),
+//            this, SLOT(syncRequestFinished(QNetworkReply*)));
+
+//    QByteArray data = QtJson::Json::serialize(collectSyncData());
+
+//    // FIXME for debug
+//    qDebug() << "Sync" << QString::fromUtf8(data.data(), data.size());
+
+//    manager->post(request, data);
+    return true;
 }
--- panexapi/panexapi.h
+++ panexapi/panexapi.h
@@ -4,18 +4,26 @@
 #include <QObject>
 #include "global_include.h"
 
+
 class PanexApi : public QObject
 {
     Q_OBJECT
 public:
     explicit PanexApi(QObject *parent = 0);
-    
+    static PanexApi* instance();
+    bool SignUpUser(QString userName, QString userPassword, QString userRole, QString userEmail);
+
 signals:
     
 public slots:
 private:
-    static const QString *networkUrl;
+    static const QString UrlPanex;
+    static const QString UrlUserSignUp;
+    static const QString UrlUserLogin;
     static const bool isDebug;
+
+    // Singleton
+    static PanexApi* m_panex_api;
 };
 
 #endif // PANEXAPI_H
